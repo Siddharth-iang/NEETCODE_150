ae91f429765229a0d0834db8fbea9c5df1208133
Siddharth-iang <siddureddyvr1115@gmail.com>
2026-02-05 05:01:46 +0530

    Refactor topKFrequent method to use a single map

--- TopKFreqEle.java
+++ TopKFreqEle.java
@@ -1,29 +1,34 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer> freqMap = new HashMap<>();
+        
+         HashMap<Integer,Integer> map = new HashMap<>();
 
         for(int num: nums){
-            freqMap.put(num, freqMap.getOrDefault(num,0) + 1);
+            map.put(num, map.getOrDefault(num,0) + 1);
         } 
 
+        //Create a min-heap and sort in A.O
         PriorityQueue<Map.Entry<Integer,Integer>> minHeap = new PriorityQueue<>(
             (a,b) -> a.getValue() - b.getValue()
         );
 
-        for(Map.Entry<Integer,Integer> entry: freqMap.entrySet()){
+        //Push all elements in hashmap to min-heap and remove top elements > k
+        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
             minHeap.add(entry);
 
             if(minHeap.size() > k){
                 minHeap.poll();
             }
         }
-
+        
+        //Add all the ele in min-heap to result[]
         int result[] = new int[k];
 
-        int i =0;
+        int i=0;
         while(!minHeap.isEmpty()){
             result[i++] = minHeap.poll().getKey();
         }
+
         return result;
     }
 }
