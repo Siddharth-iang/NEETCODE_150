e05fbe945b5f946192d37270659726b3cda5936a
Siddharth-iang <siddureddyvr1115@gmail.com>
2026-02-05 05:01:01 +0530

    Implement isAnagram with two hashmaps
    Added a second implementation of the isAnagram method using two hashmaps for comparison.

--- ValidAnagram.java
+++ ValidAnagram.java
@@ -1,3 +1,5 @@
+//Using one hashmap
+
 class Solution {
     public boolean isAnagram(String s, String t) {
 
@@ -22,5 +24,36 @@ class Solution {
             }
         }
         return true;
+    }
+}
+
+//Using 2 hashmap
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length()!=t.length()){
+            return false;
+        }
+
+        HashMap<Character,Integer> map1 = new HashMap<>();
+        HashMap<Character,Integer> map2 = new HashMap<>();
+
+        for(char c: s.toCharArray()){
+            map1.put(c, map1.getOrDefault(c,0) + 1);
+        }
+
+        for(char c: t.toCharArray()){
+            map2.put(c, map2.getOrDefault(c,0)+1);
+        }
+
+        int sum1 = 0;
+        for(char c: map1.keySet()){
+            sum1 = sum1 + map1.get(c);
+        }
+
+        if(map1.equals(map2)){
+            return true;
+        }
+        return false;
     }
 }
